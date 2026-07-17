be6b2d17b45d6c52cfc662fc194a38a416b24515
Cosmos <92128982+thecosmoscornerxyz@users.noreply.github.com>
2026-07-17 14:16:57 -0700

    July 17th Linux Daily Practice

---
+++ 2026/july/17/systemd/system.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+SERVICE=cups
+
+echo "$SERVICE status"
+systemctl status "$SERVICE" --no-pager
+echo
+echo "Restarting"
+sudo systemctl restart "$SERVICE"
+echo
+echo "Enable at Boot"
+sudo systemctl enable "$SERVICE"
+echo
+echo "Last 10 Log Lines"
+journalctl -u "$SERVICE" -n 10 --no-pager
+echo
+echo "Disable"
+sudo systemctl disable "$SERVICE"
+echo
+echo "Stop"
+sudo systemctl stop "$SERVICE"
+echo
+echo "Final status"
+systemctl status "$SERVICE" --no-pager
