fc092dba83fc8a5535ce358aa9e0e18ed0888530
Cosmos <92128982+thecosmoscornerxyz@users.noreply.github.com>
2026-07-19 15:52:45 -0700

    July 19th Linux Daily Practice

---
+++ 2026/july/19/system/system.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Linux Warmup"
+echo 
+echo "User: $(whoami)"
+echo "Hostname: $(hostname)"
+echo "uptime: $(uptime -p)"
+echo "Today is: $(date)"
+echo
+echo "Disk Usage:"
+df -h /
+echo
+echo "IP Address:"
+ip -brief addr
+echo
+echo "SSH Service:"
+systemctl is-active ssh || systemctl status ssh | head -n 10
