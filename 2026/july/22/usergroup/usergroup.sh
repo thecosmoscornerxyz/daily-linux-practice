adfba9cc360c72b46613b3b450c1c4e267246b9a
Cosmos <92128982+thecosmoscornerxyz@users.noreply.github.com>
2026-07-22 14:38:11 -0700

    July 22nd Linux Daily Practice

---
+++ 2026/july/22/usergroup/usergroup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sudo useradd -m tempuser01
+sudo groupadd -f tempgroup
+sudo usermod -aG tempgroup tempuser01
+
+echo "tempuser01 created"
+grep tempuser01 /etc/passwd
+echo groups
+id tempuser01
+echo
+echo "passwd entry"
+grep tempuser01 /etc/passwd
+echo "shadow"
+sudo grep tempuser01 /etc/shadow
+echo
+echo "tempuser01 deleted"
+sudo userdel -r tempuser01
+grep tempuser01 /etc/passwd
