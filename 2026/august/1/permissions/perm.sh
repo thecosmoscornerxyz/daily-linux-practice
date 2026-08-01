f98f98e23f59d6affed5e607e669725b805f77d7
Cosmos <92128982+thecosmoscornerxyz@users.noreply.github.com>
2026-08-01 06:49:41 -0700

    August 1st Linux Daily Practice

---
+++ 2026/august/1/permissions/perm.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+rm -f file1 file2
+touch file1 file2
+
+echo "before"
+ls -l
+echo
+
+chmod 600 file1
+chmod 754 file2
+chown root:root file1
+chown "$USER":"$USER" file2
+
+echo "after"
+ls -l
+echo
+
+echo "file1 should be r-------"
+echo "file2 should be rwxr-xr--"
