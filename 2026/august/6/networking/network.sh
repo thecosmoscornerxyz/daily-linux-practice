8d1fb985494b74861349017fb4b4f8546a1dd973
Cosmos <92128982+thecosmoscornerxyz@users.noreply.github.com>
2026-08-06 15:23:12 -0700

    August 6th Linux Daily Practice

---
+++ 2026/august/6/networking/network.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "ip"
+ip -brief addr
+echo
+echo "route"
+ip route
+echo
+echo "DNS"
+dig + short google.com | nslookup google.com
+echo
+echo "TCPDUMP"
+sudo timeout 10 tcpdump -n -i any | head -n 30
+echo 
+echo "open ports"
+ss -tulnp | head -n 10
+echo
+echo "open ports"
+ss -tulnp | head -n 10
+echo
+echo "nmap"
+sudo nmap -Pn 127.0.0.1
+echo
+echo "ARP"
+ip neigh
